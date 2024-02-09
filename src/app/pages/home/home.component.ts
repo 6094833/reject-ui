7b46763ac69338c8936e3be8ea550b44b0229c88
6094833 <-g>
2024-02-09 13:19:54 -0500

    Done mat-accordian visibilty toggle on location code search
    Done design for query reject scan inputs/controls

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 import { MatAccordion } from '@angular/material/expansion';
 import { ViewChild } from '@angular/core';
 @Component({
@@ -10,6 +10,39 @@ import { ViewChild } from '@angular/core';
 export class HomeComponent {
 
   @ViewChild(MatAccordion) accordion: MatAccordion | undefined;
-  locationControl = new FormControl('');
-  
+  locationControl:FormControl;
+  queryRejectScanForm:FormGroup;
+  isLocationSearched:boolean;
+
+  public constructor(
+    private fb: FormBuilder
+  ) {
+    this.isLocationSearched = false;
+    this.locationControl = new FormControl("", Validators.required);
+    this.queryRejectScanForm = this.fb.group({
+      locationId : [""],
+      fromDate : ["", Validators.required],
+      toDate : ["", Validators.required],
+      fedexId : [""],
+      orgCd : [""]
+    });
+  }
+
+  queryRejectScanFormOnSubmit() {
+    if(!this.queryRejectScanForm.valid) {
+      console.log("Enter all required fields!");
+      return;
+    } 
+    console.log("Form values : ", this.queryRejectScanForm.value);
+  }
+
+  onLocationCodeSearch() {
+    if(!this.locationControl.valid){
+      console.log("Please enter location code!");
+      return;
+    }
+    this.isLocationSearched = true;
+    console.log("Location Code : ",this.locationControl.value);
+  }
+
 }
